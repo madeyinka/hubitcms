b9f0caac8defeeb8b4f12490588abb3f000944ab
madeyinka007 <mutairadeyinka@yahoo.com>
2021-12-23 07:33:49 +0100

    middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,26 +1,27 @@
 const jwt = require('jsonwebtoken')
+const clientModel = require('./../model/ClientModel')
 const dotenv = require('dotenv').config()
 
 const init = {
 
     authenticate: (req, res, next) => {
         if (!req.headers.authorization) {
-            return res.status(401).json({message: "Authorization required"})
+            res.status(401).json({message: "Authorization required"})
         }
         let splitHeader = req.headers.authorization.split(' ')
         if (splitHeader[0] !== 'Bearer') {
-            return res.status(401).json({message: "Invalid token format"})
+            res.status(401).json({message: "Invalid token format"})
         }
         let token = splitHeader[1];
-        jwt.verify(token, process.env.JWT_SECRET, (error, decodedToken) => {
-            if (error && error.name == 'TokenExpiredError') {
-                return res.status(401).json({message:"Expired Token"})
+        jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+            if (error) res.status(401).json({message:"Not authorized"})
+            else{
+                clientModel.findOne({conditions:{_id:decoded.id}}, (user) => {
+                    if (!user) throw new Error()
+                    req.userInfo = user
+                    next()
+                })
             }
-            if (!decodedToken ) {
-                return res.status(401).json({message: "Invalid authorization token"})
-            }
-            req.userInfo = decodedToken
-            next()
         })
     }
 }
